d069c0e303170238318e2675bb19bca9e0a7dd16
ciellove0811 <ciellove0811@icloud.com>
2021-12-01 12:13:03 +0900

    [Update]2-2(+JS④)

--- chapter2-2/omikuji.js
+++ chapter2-2/omikuji.js
@@ -5,4 +5,7 @@ let userresult;
 username = prompt("お名前を教えて下さい。");
 document.getElementById("name").innerHTML = username;
 
-let rand = Math.floor( Math.random() * 5) ;
+let rand = Math.floor( Math.random() * 5);
+if (rand == 0) {
+  userresult = "大吉";
+}
